2d4c8aa5e413cf7fee39add8996541a83b80fe91
xioren <40043405+xioren@users.noreply.github.com>
2024-01-16 12:57:57 -0800

    Update blake2b.nim

--- blake2b.nim
+++ blake2b.nim
@@ -174,7 +174,7 @@ proc initBlake2bCtx(ctx: var Blake2bCtx, key, salt, personal: openArray[byte], d
     doAssert(personal.len <= maxPersonSize, "personalization size exceeds maximum $1 bytes" % $maxPersonSize)
     copyMem(addr P[6], unsafeAddr personal[0], personal.len)
   
-  # NOTE: XOR the first 8 words of the state with the parameter block
+  # NOTE: XOR the first 8 words of the parameter block with the state
   for i in 0 ..< wordsInState:
     ctx.state[i] = ctx.state[i] xor P[i]
   
